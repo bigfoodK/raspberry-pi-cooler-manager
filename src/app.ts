1590480fdddaeaf2733467d3207113e710009498
bigfoodK <38313680+bigfoodK@users.noreply.github.com>
2019-05-19 03:53:41 +0900

    Add temerature-observer (#2)

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,7 @@
-console.log('hello world!!');
+import TemperatureObserver from "./TemperatureObserver";
+
+const temperatureObserver = new TemperatureObserver();
+
+temperatureObserver.on('onTempChange', (temp, oldTemp) => {
+  console.log(temp, oldTemp, temp + oldTemp);
+});
